285975ec9ad7bfb1cdbb4fa07bdade1ab6d2059f
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2025-01-31 14:10:21 +0100

    styling layout and cards

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,13 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Pixel Positions</title>
 </head>
-<body>
-    <div>
-        <nav></nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
 
-        <main>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>
+                <a href="#">Post a Job</a>
+            </div>
+        </nav>
+
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
         </main>
     </div>
